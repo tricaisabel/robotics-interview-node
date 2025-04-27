7f3bad871e458dac937ab309287b50e2d643bb65
mero-isabel-trica <149565474+mero-isabel-trica@users.noreply.github.com>
2025-04-27 17:56:29 +0300

    remove dist files

--- dist/models/sessionModel.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-const sessionSchema = new mongoose.Schema({
-    started: { type: Date, required: true },
-    ended: { type: Date, default: null },
-    detections: { type: Number, default: 0 },
-    resolved: { type: Number, default: 0 },
-});
-export const SessionModel = mongoose.model("Session", sessionSchema);
